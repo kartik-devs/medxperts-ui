37bc1072e9a57b071bb4ec3c7890b4f731d1e13c
kartik-devs <kartik711111@gmail.com>
2026-01-12 16:49:23 +0530

    Update preview settings in vite.config.js
    Add allowed hosts for preview configuration

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,11 @@ export default defineConfig({
   },
   preview: {
     port: process.env.PORT || 4173,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    allowedHosts: [
+      'medxperts-frountend.onrender.com',
+      'medxperts-frontend.onrender.com',
+      '.onrender.com'
+    ]
   }
 })
